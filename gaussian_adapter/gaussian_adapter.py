e623cca6f08a0c32ecb52955cff3bba3a33f70d7
stonecodecs <slystone20@gmail.com>
2026-02-01 01:25:24 +0000

    [ADD] finish gaussian adapter logic* (untested)

--- gaussian_adapter/gaussian_adapter.py
+++ gaussian_adapter/gaussian_adapter.py
@@ -1,14 +1,18 @@
 from dataclasses import dataclass
-from gaussians import Gaussians, construct_covariance_matrix
+from gaussians import Gaussians, construct_covariance_matrix, rotate_sh
 import torch
 import torch.nn.functional as F
+from einops import rearrange
+from einops import einsum
+from typing import Tuple
+
 
 @dataclass
 class GaussianAdapterCfg:
     sh_degree: int = 3
     scale_min: float = 0.01
     scale_max: float = 100
-    # add more stuff later if needed
+    gaussian_scale_pct: float = 0.1  # heuristic used in orig. MVSplat
 
 def to_homogeneous(points: torch.Tensor, at_infinity: bool = False) -> torch.Tensor:
     concat_array = torch.ones_like(points[..., :1])
@@ -17,13 +21,13 @@ def to_homogeneous(points: torch.Tensor, at_infinity: bool = False) -> torch.Ten
     return torch.cat([points, concat_array], dim=-1)
 
 # maybe move this to a separate file
-def get_camera_rays(
+def get_camera_rays_world(
     pixel_centers: torch.Tensor,
     extrinsics: torch.Tensor,
     intrinsics: torch.Tensor,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     """
-    Get rays extending from camera center to pixel centers, in world space.
+    Get rays extending from camera center to pixel centers, in *world space*.
 
     Input:
     - pixel_centers: (B, G, 2)
@@ -89,8 +93,16 @@ class GaussianAdapter(torch.nn.Module):
         scales, quaternions, sh = torch.split(pre_gaussians, (3,4,3 * self.sh_dim), dim=2)
 
         # bound and squeeze depths into a certain scene range
+        # depths used for perspective projection (as a pixel covers more space further away)
         scene_range = self.cfg.scale_max - self.cfg.scale_min
         scales = (self.cfg.scale_min + scene_range * torch.sigmoid(scales)) * depths.unsqueeze(-1)
+        pixel_size = 1. / torch.tensor([W, H], device=depths.device)
+        scale_adjustment = self.cfg.gaussian_scale_pct * einsum(
+            intrinsics[..., :2, :2].inverse(), # focal lengths
+            pixel_size,
+            "...ij,j->..."
+        )
+        scales = scales * scale_adjustment.unsqueeze(-1)
 
         # normalize to get actual quaternion from "quaternion logits"
         quaternions = F.normalize(quaternions, dim=-1)
@@ -101,13 +113,13 @@ class GaussianAdapter(torch.nn.Module):
         covariances = c2w @ covariances @ c2w.T
 
         # gaussian means from 'ray tracing'/unprojecting based on depth
-        ray_o, ray_d = get_camera_rays(pixel_centers, extrinsics, intrinsics)
+        ray_o, ray_d = get_camera_rays_world(pixel_centers, extrinsics, intrinsics)
         means = ray_o + ray_d * depths.unsqueeze(-1)
 
         # spherical harmonics need to be expanded and set to world space
         sh = rearrange(sh, '... (x sh_dim) -> ... x sh_dim', sh_dim=self.sh_dim)
         sh = sh.expand(B, G, -1, -1)
-        # [TODO: need to rotate SH] sh = rotate_sh(sh, c2w) 
+        sh = rotate_sh(sh, c2w)
 
         return Gaussians(
             means=means,
--- gaussian_adapter/gaussians.py
+++ gaussian_adapter/gaussians.py
@@ -1,6 +1,9 @@
 import torch
 from dataclasses import dataclass
 from einops import rearrange
+from e3nn.o3 import matrix_to_angles, wigner_D
+from einops import einsum
+from math import sqrt
 
 @dataclass
 class Gaussians:
@@ -37,6 +40,7 @@ def quaternion_to_matrix(
     )
     return rearrange(o, "... (i j) -> ... i j", i=3, j=3)
 
+
 def construct_covariance_matrix(
     scale: torch.Tensor,
     quaternion: torch.Tensor
@@ -44,3 +48,20 @@ def construct_covariance_matrix(
     scale = scale * torch.eye(scale).expand(scale.shape[:-2], -1, -1)
     rotation = quaternion_to_matrix(quaternion)
     return rotation @ scale @ scale.T @ rotation.T
+
+
+def rotate_sh(sh: torch.Tensor, rotations: torch.Tensor) -> torch.Tensor:
+    # Wigner-D, basically rotations for spherical harmonics
+    n = sh.shape[-1]
+
+    alpha, beta, gamma = matrix_to_angles(rotations)
+    rotated_sh_list = []
+    for deg in range(int(sqrt(n))):
+        sh_rotations = wigner_D(deg, alpha, beta, gamma).to(sh.device).to(sh.dtype)
+        sh_rotated = einsum(
+            sh_rotations,
+            sh[..., deg ** 2 : (deg + 1) ** 2],
+            "...ij, ...j -> ...i"
+        )
+        rotated_sh_list.append(sh_rotated)
+    return torch.cat(rotated_sh_list, dim=-1)
