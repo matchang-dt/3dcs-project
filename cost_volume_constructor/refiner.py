2f36bdfe5712fde187eaa66a745a7636684e6659
Stone Yang <slystone20@gmail.com>
2026-02-09 05:32:27 +0000

    [ADD] borrow from main branch refiner updates to prevent nan

--- cost_volume_constructor/refiner.py
+++ cost_volume_constructor/refiner.py
@@ -2,49 +2,26 @@ import lightning as L
 import torch
 from torch import nn
 
-from utils import WindowSelfAttention, WindowCrossAttention, patchify
-
-
-class ResBlock4UNet(L.LightningModule):
-    def __init__(self, in_channels, out_channels, dtype=torch.float32):
-        assert in_channels % 16 == 0, "in_channels must be divisible by 16 for group normalization"
-        assert out_channels % 16 == 0, "out_channels must be divisible by 16 for group normalization"
-        super().__init__()
-        num_groups1 = in_channels // 16
-        num_groups2 = out_channels // 16
-        self.conv1 = nn.Conv2d(in_channels, out_channels, 3, stride=1, padding=1, bias=False, dtype=dtype)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1, bias=False, dtype=dtype)
-        self.silu = nn.SiLU()
-        self.gn1 = nn.GroupNorm(num_groups=num_groups1, num_channels=in_channels, dtype=dtype)
-        self.gn2 = nn.GroupNorm(num_groups=num_groups2, num_channels=out_channels, dtype=dtype)
-        nn.init.kaiming_normal_(self.conv1.weight, mode='fan_out', nonlinearity='relu')
-        nn.init.kaiming_normal_(self.conv2.weight, mode='fan_out', nonlinearity='relu')
-        nn.init.constant_(self.gn1.weight, 1)
-        nn.init.constant_(self.gn2.weight, 1)
-        nn.init.constant_(self.gn1.bias, 0)
-        nn.init.constant_(self.gn2.bias, 0)
-
-        self.skip = nn.Identity()
-        if in_channels != out_channels:
-            self.skip = nn.Conv2d(in_channels, out_channels, 1, stride=1, padding=0, bias=False, dtype=dtype)
-            nn.init.kaiming_normal_(self.skip.weight, mode='fan_out', nonlinearity='relu')
-
-    def forward(self, x):
-        out = self.gn1(x)
-        out = self.silu(out)
-        out = self.conv1(out)
-        out = self.gn2(out)
-        out = self.silu(out)
-        out = self.conv2(out)
-        out = out + self.skip(x) # skip connection
-        return out
+from utils import SwinCrossBlock, ResBlock4UNet, patchify
 
 
 class CostVolumeRefiner(L.LightningModule):
+    """
+    Cost volume refiner module.
+    Refines the cost volume with the features and the images by a U-net based refiner.
+    """
     def __init__(self, channels=128, feat_map_size=64, dtype=torch.float32):
+        """
+        Initialize the CostVolumeRefiner.
+        Args:
+            channels (int): number of channels for the features
+            feat_map_size (int): size of the feature map
+            dtype (torch.dtype): data type
+        """
         super().__init__()
-        self.channels = channels
         self.to(dtype)
+        self.channels = channels
+        num_groups = channels // 16
         self.res_enc1_1 = ResBlock4UNet(channels*2, channels, dtype)
         self.res_enc1_2 = ResBlock4UNet(channels, channels, dtype)
         self.down_conv1 = nn.Conv2d(channels, channels, 3, stride=2, padding=1, bias=False, dtype=dtype)
@@ -57,17 +34,29 @@ class CostVolumeRefiner(L.LightningModule):
         self.up_conv2 = nn.ConvTranspose2d(channels, channels, 4, stride=2, padding=1, bias=False, dtype=dtype)
         self.res_dec2_1 = ResBlock4UNet(channels*2, channels, dtype)
         self.res_dec2_2 = ResBlock4UNet(channels, channels, dtype)
-        self.self_attn = WindowSelfAttention(channels, window_size=feat_map_size//4, num_heads=1, dtype=dtype) # only 1 window: not swinT
-        self.cross_attn1 = WindowCrossAttention(channels, window_size=feat_map_size//4, num_heads=1, dtype=dtype) # only 1 window: not swinT
-        self.cross_attn2 = WindowCrossAttention(channels, window_size=feat_map_size//4, num_heads=1, dtype=dtype) # only 1 window: not swinT
-        self.cross_attn3 = WindowCrossAttention(channels, window_size=feat_map_size//4, num_heads=1, dtype=dtype) # only 1 window: not swinT
+        self.cross_block1 = SwinCrossBlock(channels, window_size=feat_map_size//4, shift_size=0, dtype=dtype) # only 1 window: not swinT
+        self.cross_block2 = SwinCrossBlock(channels, window_size=feat_map_size//4, shift_size=0, dtype=dtype) # only 1 window: not swinT
+        self.cross_block3 = SwinCrossBlock(channels, window_size=feat_map_size//4, shift_size=0, dtype=dtype) # only 1 window: not swinT
+        self.final_gn = nn.GroupNorm(num_groups=num_groups, num_channels=channels, dtype=dtype)
+        self.silu = nn.SiLU()
+        self.final_conv = nn.Conv2d(channels, channels, 1, stride=1, padding=0, bias=True, dtype=dtype)
         nn.init.kaiming_normal_(self.down_conv1.weight, mode='fan_out', nonlinearity='relu')
         nn.init.kaiming_normal_(self.down_conv2.weight, mode='fan_out', nonlinearity='relu')
         nn.init.kaiming_normal_(self.up_conv1.weight, mode='fan_out', nonlinearity='relu')
         nn.init.kaiming_normal_(self.up_conv2.weight, mode='fan_out', nonlinearity='relu')
-
+        nn.init.kaiming_normal_(self.final_conv.weight, mode='fan_out', nonlinearity='relu')
+        nn.init.constant_(self.final_conv.bias, 0)
+        nn.init.constant_(self.final_gn.weight, 1)
+        nn.init.constant_(self.final_gn.bias, 0)
 
     def forward(self, x):
+        """
+        Forward pass of the CostVolumeRefiner.
+        Args:
+            x (torch.Tensor): input tensor of shape [B, K, H//4, W//4, d (128*2)] concat of upsampled features (128) and cost volume (128)
+        Returns:
+            out (torch.Tensor): output tensor of shape [B, K, H//4, W//4, 128]
+        """
         # x: [B, K, H//4, W//4, d (128*2)]
         b, k, h, w, d = x.shape
         x = x.permute(0, 1, 4, 2, 3).reshape(b*k, d, h, w) # [B, K, H//4, W//4, d] -> [B*K, d, H//4, W//4]
@@ -81,13 +70,12 @@ class CostVolumeRefiner(L.LightningModule):
 
         # bottleneck
         h3 = patchify(h3.reshape(b, k, self.channels, h//4, w//4)) # [B*K, K=(src|tgt), H//16, W//16, 128]
-        h_src = h3[:, 0, :, :, :].reshape(b*k, h//4 * w//4, self.channels) # [B*K, H//16, W//16, 128]
-        h_tgt = h3[:, 1:, :, :, :].reshape(b*k, (k-1) * h//4 * w//4, self.channels) # [B*K, K-1, H//16, W//16, 128]
-        h_src = self.self_attn(h_src)
-        h_src = self.cross_attn1(h_src, h_tgt)
-        h_src = self.cross_attn2(h_src, h_tgt)
-        h_src = self.cross_attn3(h_src, h_tgt) # [B*K, H//16*W//16, 128]
-        h3 = h_src.permute(0, 2, 1).reshape(b*k, self.channels, h//4, w//4) # [B*K, 128, H//16, W//16]
+        h_src = h3[:, 0, :, :, :].reshape(b*k, h//4, w//4, self.channels) # [B*K, H//16, W//16, 128]
+        h_tgt = h3[:, 1:, :, :, :].reshape(b*k, (k-1), h//4, w//4, self.channels) # [B*K, K-1, H//16, W//16, 128]
+        h_src = self.cross_block1(h_src, h_tgt)
+        h_src = self.cross_block2(h_src, h_tgt)
+        h_src = self.cross_block3(h_src, h_tgt) # [B*K, H//16, W//16, 128]
+        h3 = h_src.permute(0, 3, 1, 2) # [B*K, 128, H//16, W//16]
 
         # decoder
         h4 = self.up_conv1(h3) # [B*K, 128, H//8, W//8]
@@ -98,5 +86,8 @@ class CostVolumeRefiner(L.LightningModule):
         h5 = torch.cat([h1, h5], dim=1) # [B*K, 256, H//4, W//4]
         out = self.res_dec2_1(h5) # [B*K, 128, H//4, W//4]
         out = self.res_dec2_2(out) # [B*K, 128, H//4, W//4]
+        out = self.final_gn(out)
+        out = self.silu(out)
+        out = self.final_conv(out)
         out = out.permute(0, 2, 3, 1).reshape(b, k, h, w, self.channels) # [B, K, H//4, W//4, 128]
         return out
--- depth_estimator/refiner.py
+++ depth_estimator/refiner.py
@@ -2,17 +2,28 @@ import lightning as L
 import torch
 from torch import nn
 
-from utils import WindowSelfAttention, WindowCrossAttention, patchify
-from utils import ResBlock4UNet
+from utils import SwinCrossBlock, ResBlock4UNet, patchify
 
 
 class DepthRefiner(L.LightningModule):
+    """
+    Depth refiner module.
+    Refines the depth map with the features and the images by a U-net based refiner.
+    U-Net of 4 stages, transformer blocks in the bottleneck.
+    """
     def __init__(self, channels=128, feat_map_size=256, dtype=torch.float32):
+        """
+        Initialize the DepthRefiner.
+        Args:
+            channels (int): number of channels for the features
+            feat_map_size (int): size of the feature map
+            dtype (torch.dtype): data type
+        """
         assert feat_map_size % 16 == 0
         super().__init__()
         self.to(dtype)
         self.channels = channels
-
+        num_groups = channels // 16
         self.in_conv = nn.Conv2d(channels+4, channels, 3, stride=1, padding=1, bias=False, dtype=dtype)
         self.res_enc1_1 = ResBlock4UNet(channels, channels, dtype)
         self.res_enc1_2 = ResBlock4UNet(channels, channels, dtype)
@@ -38,11 +49,12 @@ class DepthRefiner(L.LightningModule):
         self.up_conv4 = nn.ConvTranspose2d(channels, channels, 4, stride=2, padding=1, bias=False, dtype=dtype)
         self.res_dec4_1 = ResBlock4UNet(channels*2, channels, dtype)
         self.res_dec4_2 = ResBlock4UNet(channels, channels, dtype)
-        self.self_attn = WindowSelfAttention(channels, window_size=feat_map_size//16, num_heads=1, dtype=dtype) # only 1 window: not swinT
-        self.cross_attn1 = WindowCrossAttention(channels, window_size=feat_map_size//16, num_heads=1, dtype=dtype) # only 1 window: not swinT
-        self.cross_attn2 = WindowCrossAttention(channels, window_size=feat_map_size//16, num_heads=1, dtype=dtype) # only 1 window: not swinT
-        self.cross_attn3 = WindowCrossAttention(channels, window_size=feat_map_size//16, num_heads=1, dtype=dtype) # only 1 window: not swinT
-        self.last_conv = nn.Conv2d(channels, 1, kernel_size=3, stride=1, padding=1, bias=True, dtype=dtype) # ch 128 -> 1
+        self.cross_block1 = SwinCrossBlock(channels, window_size=feat_map_size//16, shift_size=0, dtype=dtype) # only 1 window: not swinT
+        self.cross_block2 = SwinCrossBlock(channels, window_size=feat_map_size//16, shift_size=0, dtype=dtype) # only 1 window: not swinT
+        self.cross_block3 = SwinCrossBlock(channels, window_size=feat_map_size//16, shift_size=0, dtype=dtype) # only 1 window: not swinT
+        self.final_gn = nn.GroupNorm(num_groups=num_groups, num_channels=channels, dtype=dtype)
+        self.silu = nn.SiLU()
+        self.final_conv = nn.Conv2d(channels, 1, kernel_size=1, stride=1, padding=0, bias=True, dtype=dtype) # ch 128 -> 1
 
         nn.init.kaiming_normal_(self.in_conv.weight, mode='fan_out', nonlinearity='relu')
         nn.init.kaiming_normal_(self.down_conv1.weight, mode='fan_out', nonlinearity='relu')
@@ -53,10 +65,19 @@ class DepthRefiner(L.LightningModule):
         nn.init.kaiming_normal_(self.up_conv2.weight, mode='fan_out', nonlinearity='relu')
         nn.init.kaiming_normal_(self.up_conv3.weight, mode='fan_out', nonlinearity='relu')
         nn.init.kaiming_normal_(self.up_conv4.weight, mode='fan_out', nonlinearity='relu')
-        nn.init.kaiming_normal_(self.last_conv.weight, mode='fan_out', nonlinearity='relu')
-        nn.init.constant_(self.last_conv.bias, 0)
+        nn.init.kaiming_normal_(self.final_conv.weight, mode='fan_out', nonlinearity='relu')
+        nn.init.constant_(self.final_conv.bias, 0)
+        nn.init.constant_(self.final_gn.weight, 1)
+        nn.init.constant_(self.final_gn.bias, 0)
 
     def forward(self, x):
+        """
+        Forward pass of the DepthRefiner.
+        Args:
+            x (torch.Tensor): input tensor of shape [B, K, H, W, d (128 + 4)] 128: upsampled features, 3: images, 1: depth map
+        Returns:
+            out (torch.Tensor): output tensor of shape [B, K, H, W]
+        """
         # x: [B, K, H, W, d (128 + 4)]
         b, k, H, W, d = x.shape
         assert d == self.channels + 4
@@ -78,13 +99,12 @@ class DepthRefiner(L.LightningModule):
 
         # bottleneck
         h5 = patchify(h5.reshape(b, k, self.channels, H//16, W//16)) # [B*K, K=(src|tgt), H//16, W//16, 128]
-        h_src = h5[:, 0, :, :, :].reshape(b*k, H//16 * W//16, self.channels) # [B*K, H//16, W//16, 128]
-        h_tgt = h5[:, 1:, :, :, :].reshape(b*k, (k-1) * H//16 * W//16, self.channels) # [B*K, (K-1) * H//16 * W//16, 128]
-        h_src = self.self_attn(h_src)
-        h_src = self.cross_attn1(h_src, h_tgt)
-        h_src = self.cross_attn2(h_src, h_tgt)
-        h_src = self.cross_attn3(h_src, h_tgt) # [B*K, H//16*W//16, 128]
-        h5 = h_src.permute(0, 2, 1).reshape(b * k, self.channels, H//16, W//16) # [B*K, 128, H//16, W//16]
+        h_src = h5[:, 0, :, :, :].reshape(b*k, H//16, W//16, self.channels) # [B*K, H//16, W//16, 128]
+        h_tgt = h5[:, 1:, :, :, :].reshape(b*k, (k-1), H//16, W//16, self.channels) # [B*K, (K-1) * H//16 * W//16, 128]
+        h_src = self.cross_block1(h_src, h_tgt)
+        h_src = self.cross_block2(h_src, h_tgt)
+        h_src = self.cross_block3(h_src, h_tgt) # [B*K, H//16, W//16, 128]
+        h5 = h_src.permute(0, 3, 1, 2) # [B*K, 128, H//16, W//16]
 
         # decoder
         h6 = self.up_conv1(h5) # [B*K, 128, H//8, W//8]
@@ -103,6 +123,8 @@ class DepthRefiner(L.LightningModule):
         h9 = torch.cat([h1, h9], dim=1) # [B*K, 256, H, W]
         h9 = self.res_dec4_1(h9) # [B*K, 128, H, W]
         h9 = self.res_dec4_2(h9) # [B*K, 128, H, W]
-        out = self.last_conv(h9) # [B*K, 1, H, W]
+        out = self.final_gn(h9) # [B*K, 128, H, W]
+        out = self.silu(out)
+        out = self.final_conv(out) # [B*K, 1, H, W]
         out = out.permute(0, 2, 3, 1).reshape(b, k, H, W) # [B, K, H, W]
         return out # [B, K, H, W]
